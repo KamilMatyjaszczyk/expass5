8ea883235d8878b3cde1c41468775c68dad86b21
KamilMatyjaszczyk <Kamilmaty@hotmail.com>
2025-09-24 14:08:46 +0200

    spring boot

---
+++ src/main/java/com/example/expass5/Expass5Application.java
@@ -0,0 +1,13 @@
+package com.example.expass5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Expass5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Expass5Application.class, args);
+    }
+
+}
